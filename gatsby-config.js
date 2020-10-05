71e2d70c24411bc63c3b706453b24202b153a677
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-10-05 15:26:52 +0200

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-hello-world.git)

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-theme-material-ui`,
+      options: {
+        stylesConfig: {
+          // disableAutoprefixing: true,
+          disableMinification: true
+        },
+      },
+    },
+  ],
+}
