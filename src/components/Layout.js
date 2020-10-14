1c13ab8abc7bdf0f8ac3364072e59817166455c8
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-10-14 17:42:27 +0200

    Update title suffix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -37,7 +37,7 @@ const Layout = ({
       <Helmet
         htmlAttributes={{ lang: 'fr' }}
         title={title}
-        titleTemplate="%s | OutdoorGeovision"
+        titleTemplate="%s | OutdoorVision"
       />
 
       <TopBar />
