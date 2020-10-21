8fe37e3ac2e27857178ed3d2caa239be0aade552
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-10-21 17:44:39 +0200

    Improve use of theme values for Tripetto form style

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -24,13 +24,33 @@ const ContactForm = () => {
       styles: {
         mode: 'progressive',
         noBranding: true,
-        contract: { name: 'tripetto-runner-classic', version: '1.7.1' },
+        contract: { name: 'tripetto-runner-classic', version: '1.9.1' },
+
         color: theme.palette.primary.main,
+
+        font: {
+          size: theme.typography.fontSize,
+          family: theme.typography.fontFamily.split(',').shift(),
+        },
+
+        inputs: {
+          borderSize: 1,
+          roundness: theme.shape.borderRadius,
+          textColor: theme.palette.text.primary,
+          errorColor: theme.palette.error.main,
+          agreeColor: theme.palette.success.main,
+          declineColor: theme.palette.error.main,
+        },
+
+        buttons: {
+          mode: 'fill',
+          roundness: theme.shape.borderRadius,
+        },
       },
       attachments,
       onSubmit,
     });
-  }, [formWrapper, theme.palette.primary]);
+  }, [formWrapper, theme]);
 
   return (
     <div ref={formWrapper} />
