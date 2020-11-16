ebae68027f412fc9f1b86062d719524c748542e5
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-11-16 17:00:28 +0100

    Improve style for Markdown contents

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -37,7 +37,7 @@ const Layout = ({
       <Helmet
         htmlAttributes={{ lang: 'fr' }}
         title={title}
-        titleTemplate="%s | OutdoorVision"
+        titleTemplate="%s | Outdoorvision"
       />
 
       <TopBar />
--- src/components/MarkdownText.js
+++ src/components/MarkdownText.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import clsx from 'clsx';
 
 import Rehype2react from 'rehype-react';
 
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const MarkdownText = ({ hast, components, ...rest }) => {
+import Link from './Link';
+
+const useStyles = makeStyles(theme => ({
+  markdown: {
+    '& .MuiTypography-body1 + .MuiTypography-body1:not(li)': {
+      margin: theme.spacing(2, 0),
+    },
+    '& [class*="MuiTypography-h"]:not(:first-child)': {
+      marginTop: theme.spacing(4),
+    },
+  },
+}));
+
+const MarkdownText = ({ hast, components, className, ...rest }) => {
+  const classes = useStyles();
+
   const renderAst = new Rehype2react({
     createElement: React.createElement,
     Fragment: React.Fragment,
     components: {
-      h1: props => <Typography variant="h1" paragraph {...props} />,
-      h2: props => <Typography variant="h2" paragraph {...props} />,
-      h3: props => <Typography variant="h3" paragraph {...props} />,
-      h4: props => <Typography variant="h4" paragraph {...props} />,
-      h5: props => <Typography variant="h5" paragraph {...props} />,
-      h6: props => <Typography variant="h6" paragraph {...props} />,
-      p: props => <Typography variant="body1" paragraph {...props} />,
+      h1: props => <Typography variant="h1" gutterBottom {...props} />,
+      h2: props => <Typography variant="h2" gutterBottom {...props} />,
+      h3: props => <Typography variant="h3" gutterBottom {...props} />,
+      h4: props => <Typography variant="h4" gutterBottom {...props} />,
+      h5: props => <Typography variant="h5" gutterBottom {...props} />,
+      h6: props => <Typography variant="h6" gutterBottom {...props} />,
+      p: props => <Typography variant="body1" {...props} />,
       li: props => <Typography variant="body1" component="li" {...props} />,
+      a: props => <Link {...props} />,
       // eslint-disable-next-line jsx-a11y/alt-text
       img: props => <img style={{ maxWidth: '100%' }} {...props} />,
       ...components,
@@ -25,7 +42,7 @@ const MarkdownText = ({ hast, components, ...rest }) => {
   }).Compiler;
 
   return (
-    <Box {...rest}>{renderAst(hast)}</Box>
+    <Box className={clsx(classes.markdown, className)} {...rest}>{renderAst(hast)}</Box>
   );
 };
 
