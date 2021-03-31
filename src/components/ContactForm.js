d05a6ffa1b57f0f8bf208b53ea643c64628ef4a7
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-03-31 11:29:49 +0200

    Use Integromat scenario for contact form emails

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,17 +3,30 @@ import React from 'react';
 import { useTheme } from '@material-ui/core';
 import Services from 'tripetto-services';
 import { run } from 'tripetto-runner-classic';
+import { Export } from 'tripetto-runner-foundation';
 
 import { TRIPETTO_TOKEN } from '../settings';
 import definition from '../contact-form.json';
 
-const { attachments, onSubmit } = typeof window !== 'undefined'
+const { attachments } = typeof window !== 'undefined'
   ? Services.init({ token: TRIPETTO_TOKEN })
   : {};
 
 const ContactForm = () => {
   const formWrapper = React.useRef(null);
   const theme = useTheme();
+
+  const handleFormSubmit = async instance => {
+    const { fields } = Export.fields(instance);
+    const data = fields.reduce((acc, curr) => ({ ...acc, [curr.name]: curr.value }), {});
+    const body = JSON.stringify(data, null, 2);
+
+    await fetch('https://hook.integromat.com/x3bg2n9ei3l6ic63bx7ciaa5hsamdh2a', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body,
+    });
+  };
 
   React.useEffect(() => {
     if (!formWrapper || typeof window === 'undefined') return;
@@ -48,7 +61,7 @@ const ContactForm = () => {
         },
       },
       attachments,
-      onSubmit,
+      onSubmit: handleFormSubmit,
     });
   }, [formWrapper, theme]);
 
